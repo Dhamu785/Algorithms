7e412bd7acf58777d527e6bebb3c1555d0f9de8e
Dhamodharan <dhamodharan1888@gmail.com>
2023-08-13 00:21:01 +0530

    From previous tutorial

--- 01 Linked List/1.py
+++ 01 Linked List/1.py
@@ -1,1 +1,28 @@
-print("Hello world")
+# Linked List tutorials
+# Regerences : Previous tutorials
+
+class node:
+    def __init__(self,data,ref=None):
+        self.data = data
+        self.ref = ref
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def add_first(self,data):
+        nod = node(data,self.head)
+        self.head = nod
+
+    def show(self):
+        while self.head:
+            d = self.head.data
+            self.head = self.head.ref
+            print(d)
+
+
+lnk_list = LinkedList()
+for i in range(10):
+    lnk_list.add_first(i)
+
+lnk_list.show()
